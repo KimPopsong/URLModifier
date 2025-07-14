0a652a9b40eb117a82870334877f59d5cf62d102
kimds5344 <kimds5344@naver.com>
2025-07-14 20:44:38 +0900

    FEAT : URL.sql 추가

---
+++ URLModifierBackend/src/main/java/bigmac/urlmodifierbackend/sql/URL.sql
@@ -0,0 +1,17 @@
+-- Table: public.url
+
+-- DROP TABLE IF EXISTS public.url;
+
+CREATE TABLE IF NOT EXISTS public.url
+(
+    id bigint NOT NULL,
+    origin_url text COLLATE pg_catalog."default" NOT NULL,
+    shortened_url text COLLATE pg_catalog."default" NOT NULL,
+    qr_code text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT url_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.url
+    OWNER to postgres;
