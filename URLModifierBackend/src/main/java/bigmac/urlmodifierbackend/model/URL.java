dcee9f4376aeae6119381d2ec4b76b180589b86c
KimDS <kimds5344@naver.com>
2025-07-16 10:07:07 +0900

    CHORE : URL.java & User.java 수정

--- URLModifierBackend/src/main/java/bigmac/urlmodifierbackend/model/URL.java
+++ URLModifierBackend/src/main/java/bigmac/urlmodifierbackend/model/URL.java
@@ -1,20 +1,24 @@
 package bigmac.urlmodifierbackend.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "url")
 public class URL {
     @Id
     private Long id;
 
-    @Column(name = "user_id")
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @Column(name = "origin_url", nullable = false, columnDefinition = "TEXT")
     private String originURL;
@@ -22,14 +26,13 @@ public class URL {
     @Column(name = "shortened_url", nullable = false, columnDefinition = "TEXT")
     private String shortenedURL;
 
+    @Column(name = "custom_url", nullable = true, columnDefinition = "TEXT")
+    private String customURL;
+
     @Column(name = "qr_code", nullable = false, columnDefinition = "TEXT")
     private String qrCode;
 
-    public URL(Long id, String originURL, String shortenedURL, String qrCode)
-    {
-        this.id = id;
-        this.originURL = originURL;
-        this.shortenedURL = shortenedURL;
-        this.qrCode = qrCode;
-    }
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
