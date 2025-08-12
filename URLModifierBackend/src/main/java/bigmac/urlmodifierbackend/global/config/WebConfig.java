ee615ca27d1af6633240f59d6eddebe20f840832
kimds5344 <kimds5344@naver.com>
2025-08-12 21:40:26 +0900

    FIX : 순환 참조 오류 임시 해결

--- URLModifierBackend/src/main/java/bigmac/urlmodifierbackend/global/config/WebConfig.java
+++ URLModifierBackend/src/main/java/bigmac/urlmodifierbackend/global/config/WebConfig.java
@@ -1,20 +1,29 @@
 package bigmac.urlmodifierbackend.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Value("${server.front-url}")
-    private String frontUrl;
+    private String FRONT_BASE_URL;
+
+    @Bean
+    public PasswordEncoder passwordEncoder()  // TODO : SecurityConfig에 둘 시 순환 참조 문제 발생
+    {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry)
     {
         registry.addMapping("/**")  // 애플리케이션의 모든 경로에 대해 CORS 설정을 적용
-                .allowedOrigins(frontUrl)  // 요청을 허용할 출처 지정
+                .allowedOrigins(FRONT_BASE_URL)  // 요청을 허용할 출처 지정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")  // 허용할 HTTP 메서드 지정
                 .allowedHeaders("*")  // 모든 종류의 HTTP 헤더 허용
                 .allowCredentials(true)  // 자격 증명(쿠키 등)을 포함한 요청 허용
