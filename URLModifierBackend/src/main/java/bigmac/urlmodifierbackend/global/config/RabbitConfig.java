a35f96a3fde8a94bbf6afdb1092f3cc29347b26e
KimDS <kimds5344@naver.com>
2025-08-26 15:41:37 +0900

    FEAT : RabbitMQ 의존성 추가

---
+++ URLModifierBackend/src/main/java/bigmac/urlmodifierbackend/global/config/RabbitConfig.java
@@ -0,0 +1,32 @@
+package bigmac.urlmodifierbackend.global.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String QUEUE_NAME = "delete-queue";
+    public static final String EXCHANGE_NAME = "delete-exchange";
+    public static final String ROUTING_KEY = "delete.key";
+
+    @Bean
+    public Queue queue() {
+        return QueueBuilder.durable(QUEUE_NAME).build();
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+}
